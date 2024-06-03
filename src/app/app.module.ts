873ffa136b60b62b1a3c24c60a7de64b6aa03387
Mac10 <alumno@MacBook-Pro-10.local>
2024-06-03 11:41:05 -0500

    Semana 13 - DAWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CrearCategoriaComponent } from './components/crear-categoria/crear-categoria.component';
+import { ListarCategoriasComponent } from './components/listar-categorias/listar-categorias.component';
+import { CategoriaService } from './services/categoria.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent,
+    CrearCategoriaComponent,
+    ListarCategoriasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    [CategoriaService]
   ],
   bootstrap: [AppComponent]
 })
